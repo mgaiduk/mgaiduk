36b91a277df988c019ca08a8d7ebb0e958ad3f31
mgaiduk <mgaiduk@sharechat.co>
2023-03-14 19:07:47 +0000

    Fix flag parse

--- dataflow/cost_benchmark/local_wordcount/main.go
+++ dataflow/cost_benchmark/local_wordcount/main.go
@@ -21,15 +21,16 @@ var (
 )
 
 func main() {
+	flag.Parse()
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+	defer client.Close()
+	bucket := client.Bucket("mgaiduk")
+	counts := make(map[string]int64)
 	if *mode == "simple" {
-		ctx := context.Background()
-		client, err := storage.NewClient(ctx)
-		if err != nil {
-			log.Fatalf("Failed to create client: %v", err)
-		}
-		defer client.Close()
-		bucket := client.Bucket("mgaiduk")
-		counts := make(map[string]int64)
 		for i := 0; i < *count; i += 1 {
 			gcsPath := fmt.Sprintf("%v%v", *input, i)
 			rc, err := bucket.Object(gcsPath).NewReader(ctx)
@@ -47,6 +48,9 @@ func main() {
 			}
 		}
 		outputFile, err := os.Create(*output)
+		if err != nil {
+			panic(err)
+		}
 		defer outputFile.Close()
 		w := bufio.NewWriter(outputFile)
 		for word, count := range counts {
